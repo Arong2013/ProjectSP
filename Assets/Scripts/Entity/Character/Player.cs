571e40eb985c6c6b78ce4f4bbd9278e9af68b085
Arong2013 <111150462+Arong2013@users.noreply.github.com>
2024-10-07 17:31:18 +0900

    수정

---
+++ Assets/Scripts/Entity/Character/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public Player()
+    {
+        
+    }
+    public override void Init(GameObject gameObject)
+    {
+        base.Init(gameObject);
+        
+    }
+}
---
+++ Assets/Scripts/Entity/Entity.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public abstract class Entity 
+{
+    public abstract void Init(GameObject gameObject);
+}
